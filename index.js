b67b961cefdf693a3ee0b8a236e7f8153329e895
anadomuta <ana.domuta@hotmail.de>
2024-02-23 10:21:54 +0000

    adds user questions to index.js

--- index.js
+++ index.js
@@ -10,6 +10,77 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
 
+// Array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "managerName",
+    message: "Please enter your name.",
+  },
+  {
+    type: "input",
+    name: "managerId",
+    message: "Please enter your employee id.",
+  },
+  {
+    type: "input",
+    name: "managerEmail",
+    message: "Please enter your email address.",
+  },
+  {
+    type: "input",
+    name: "officeNumber",
+    message: "Please enter your office number.",
+  },
+  {
+    type: "checkbox",
+    name: "manageroOptions",
+    message: "Please select which action you would like to perform.",
+    choices: ["Add an engineer", "Add an intern", "Finish building the team"],
+  },
+  {
+    type: "input",
+    name: "engineerName",
+    message: "Please enter the engineer's name.",
+  },
+  {
+    type: "input",
+    name: "engineerId",
+    message: "Please enter the engineer's id.",
+  },
+  {
+    type: "input",
+    name: "engineerEmail",
+    message: "Please enter the engineer's email address.",
+  },
+  {
+    type: "input",
+    name: "engineerGitHub",
+    message: "Please enter the engineer's GitHub username.",
+  },
+  {
+    type: "input",
+    name: "internName",
+    message: "Please enter the intern's name.",
+  },
+  {
+    type: "input",
+    name: "internId",
+    message: "Please enter the intern's id.",
+  },
+  {
+    type: "input",
+    name: "internEmail",
+    message: "Please enter the intern's email address.",
+  },
+  {
+    type: "input",
+    name: "internSchool",
+    message: "Please enter the intern's school.",
+  },
+];
 
-// TODO: Write Code to gather information about the development team members, and render the HTML file.
-
+// Function to prompt user
+const promptUser = () => {
+  return inquirer.prompt(questions);
+};
