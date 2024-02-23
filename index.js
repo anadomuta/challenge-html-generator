4f66695c8df49ba92db69af550e73be59d26cc59
anadomuta <ana.domuta@hotmail.de>
2024-02-23 10:52:30 +0000

    adds logic to generate HTML page based on manager's input

--- index.js
+++ index.js
@@ -4,12 +4,24 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
+const util = require("util");
 const emailValidator = require("email-validator");
 
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./src/page-template.js");
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+// Function to validate user email address
+const validateEmail = (email) => {
+  if (emailValidator.validate(email)) {
+    return true;
+  } else {
+    return "Please enter a valid email address.";
+  }
+};
 
 // Array of questions for user
 const questions = [
@@ -84,16 +96,52 @@ const questions = [
   },
 ];
 
-// Function to validate user email address
-const validateEmail = (email) => {
-  if (emailValidator.validate(email)) {
-    return true;
-  } else {
-    return "Please enter a valid email address.";
-  }
-};
-
 // Function to prompt user
 const promptUser = () => {
   return inquirer.prompt(questions);
 };
+
+// Function to write to HTML
+const init = async () => {
+  try {
+    const answers = await promptUser();
+
+    // Create team member variables based on user input
+    const manager = new Manager(
+      answers.managerName,
+      answers.managerId,
+      answers.managerEmail,
+      answers.officeNumber
+    );
+    const engineer = new Engineer(
+      answers.engineerName,
+      answers.engineerId,
+      answers.engineerEmail,
+      answers.engineerGitHub
+    );
+    const intern = new Intern(
+      answers.internName,
+      answers.internId,
+      answers.internEmail,
+      answers.internSchool
+    );
+
+    const team = [manager, engineer, intern]; // create an array of team members
+
+    const pageTemplate = render(team); // generate HTML page using the previously created team array
+
+    // Creates 'output' directory if it doesn't exist
+    if (!fs.existsSync(`output`)) {
+      fs.mkdirSync(`output`);
+    }
+
+    await writeFileAsync(outputPath, pageTemplate);
+
+    console.info("Successfully wrote to team.html");
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Function call to initialize program
+init();
